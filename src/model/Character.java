dea4b1a20a876379e4148eabc3985664865206a1
emmanuel umania <dacha1252@gmail.com>
2019-09-14 11:05:27 -0500

    adding classes

---
+++ src/model/Character.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Character {
+	private String Name;
+	private String personality;
+	private String date;
+	private int power;
+	private Character next;
+	private Character prev;
+}
---
+++ src/model/Clan.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Clan {
+	private String Name;
+	private int numEstudiantes;
+	private Character firstC;
+	
+}
